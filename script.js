32663d795cb91d7bb59c8839a5341da3de5237ad
sydbarrett <isack.hossam@gmail.com>
2017-04-05 21:04:32 -0400

    fixed the google script

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-
-
-
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-25279294-1']);
-_gaq.push(['_trackPageview']);
-(function() {
-	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-78765228-3', 'auto');
+ga('send', 'pageview');
 
 var toggleclassname = function(el, classname) {
 	var children = el.parentNode.childNodes;
 	for (i in children) {
-		if (children[i] != null && children[i].className != null && children[i].className.indexOf(classname) != -1) {
-			if (children[i].style.display == 'block')
-				children[i].style.display = 'none';
-			else
-				children[i].style.display = 'block';
+		if (children[i] != null && children[i].className != null) 
+		{
+			if (children[i].className.indexOf(classname) != -1)
+			{
+				if (children[i].style.display == 'block')
+					children[i].style.display = 'none';
+				else
+					children[i].style.display = 'block';
+			}
+			//if (children[i].tagName.toLowerCase() =='pre' )
+			//	children[i].style.display = 'none';
 		}
 	}
 	return false;
